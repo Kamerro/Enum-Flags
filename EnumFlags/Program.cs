7f3d96bdbe1b3c64b462485f2c5e8c73bf4db9c3
Kamil Mikuła <kamimik090@student.polsl.pl>
2023-08-04 23:48:47 +0200

    Dodaj pliki projektów.

---
+++ EnumFlags/Program.cs
@@ -0,0 +1,18 @@
+﻿var e = Flags.User;
+e |= Flags.Super_User;
+if (e.HasFlag(Flags.Super_User))
+{
+    if (e.HasFlag(Flags.Admin))
+    {
+        Console.WriteLine("Admin");
+    }
+}
+
+[Flags]
+public enum Flags
+{
+    None = 1<<0,
+    User = 1<<1,
+    Super_User = 1<<2,
+    Admin = 1<<3,
+}
